4442551832dde4187b7dbb4ddae4a5461f2959f8
Masterrey <masterrey@gmail.com>
2020-08-06 10:55:40 -0300

    projeto

---
+++ Assets/Scripts/GamePlay.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StartGame", 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        PhotonNetwork.Instantiate("TankFree", transform.position, transform.rotation, 0);
+
+    }
+}
---
+++ Assets/Scripts/MyLobby.cs
@@ -0,0 +1,56 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLobby : MonoBehaviourPunCallbacks
+{
+    public string PlayerName;
+    public GameObject roomPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        PhotonNetwork.LocalPlayer.NickName = PlayerName;
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        roomPanel.SetActive(true);
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Não encontrou sala, Criando uma...");
+        string roomName = "Sala00";
+        RoomOptions rOp = new RoomOptions();
+        rOp.MaxPlayers = 20;
+        PhotonNetwork.CreateRoom(roomName, rOp);
+
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Level1");
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("O jogador "+newPlayer.NickName+" Entrou na Sala");
+    }
+}
