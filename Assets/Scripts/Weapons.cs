65af21a1a17f6547b0d14ff5a594d89de83c807f
Masterrey <masterrey@gmail.com>
2020-08-13 09:28:10 -0300

    bomba

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public PhotonView pview;
+    public GameObject pointoffire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pview.IsMine)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+               GameObject ob= (GameObject) 
+                    PhotonNetwork.Instantiate("Bullet", pointoffire.transform.position,
+                    pointoffire.transform.rotation, 0);
+                ob.GetComponent<Rigidbody>().AddForce(pointoffire.transform.forward * 100, ForceMode.Impulse);
+                Destroy(ob, 5);
+            }
+        }
+    }
+}
