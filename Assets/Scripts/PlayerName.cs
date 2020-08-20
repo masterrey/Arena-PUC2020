36f3798be9b5da9df321f8f301a7d4f0e9a3b1f3
Masterrey <masterrey@gmail.com>
2020-08-20 08:50:38 -0300

    Botao de ready

--- Assets/Scripts/PlayerName.cs
+++ Assets/Scripts/PlayerName.cs
@@ -7,6 +7,8 @@ public class PlayerName : MonoBehaviour
 {
     public Text nametext;
     public PhotonView pview;
+    public Toggle readyToggle;
+    public bool ready = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +17,29 @@ public class PlayerName : MonoBehaviour
         GameObject content = GameObject.FindGameObjectWithTag("PlayerList");
         transform.parent = content.transform;
 
+        if (pview.IsMine)
+        {
+            readyToggle.interactable = true;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReadyisChange()
     {
-        
+        if (ready != readyToggle.isOn)
+        {
+            ready = readyToggle.isOn;
+            pview.RPC("StatusChange", RpcTarget.OthersBuffered, ready);
+        }
     }
+    
+    [PunRPC]
+    void StatusChange(bool myready)
+    {
+        readyToggle.isOn = myready;
+
+    }
+
+
+
 }
